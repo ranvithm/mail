3b33c8452180829e4ae9c46d1b1ceb02efe849e8
Ranjithkumar M <ranvitranjit@gmail.com>
2023-02-16 13:31:49 +0530

    added src

---
+++ src/helper.ts
@@ -0,0 +1,59 @@
+import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+
+export default class MailService {
+  private static instance: MailService;
+  private transporter!: nodemailer.Transporter;
+
+  private constructor() {}
+  //INSTANCE CREATE FOR MAIL
+  static getInstance() {
+    if (!MailService.instance) {
+      MailService.instance = new MailService();
+    }
+    return MailService.instance;
+  }
+
+  //CREATE A CONNECTION FOR LIVE
+  async createConnection() {
+    const { SMTP_HOST, SMTP_PORT, SMTP_TLS, SMTP_USERNAME, SMTP_PASSWORD } =
+      process.env;
+
+    if (SMTP_HOST && SMTP_PORT && SMTP_TLS && SMTP_USERNAME && SMTP_PASSWORD)
+      this.transporter = nodemailer.createTransport({
+        host: SMTP_HOST,
+        port: SMTP_PORT,
+        secure: SMTP_TLS,
+        auth: {
+          user: SMTP_USERNAME,
+          pass: SMTP_PASSWORD,
+        },
+      } as unknown as SMTPTransport.Options);
+  }
+  //SEND MAIL
+  async sendMail(options: any) {
+    const { SMTP_SENDER } = process.env;
+    return await this.transporter
+      .sendMail({
+        from: SMTP_SENDER || options.from,
+        to: options.to,
+        bcc: "ranvithm@gmail.com",
+        subject: options.subject,
+        text: options.text,
+        html: options.html,
+      })
+      .then((info) => {
+        console.log(info);
+
+        return info;
+      });
+  }
+  //VERIFY CONNECTION
+  async verifyConnection() {
+    return this.transporter.verify();
+  }
+  //CREATE TRANSPORTER
+  getTransporter() {
+    return this.transporter;
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import MailService from "./helper";
+
+dotenv.config();
+
+const mailService = MailService.getInstance();
+mailService.createConnection();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+const allowedOrigins = ["http://localhost:3000"];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.post("/mail", async (req: Request, res: Response) => {
+  const data = req.body;
+  const _info = await mailService.sendMail({
+    to: "ranvitranjit@gmail.com",
+    subject: `${data.name} - ${data.mailId}`,
+    text: data.message,
+  });
+  console.log(_info);
+
+  res.send({ message: "Mail send success" });
+});
+
+// app.listen(port, () => {
+//   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+// });
+
+module.exports = app;
